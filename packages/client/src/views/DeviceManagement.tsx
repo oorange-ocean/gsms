08aef86c37081c5fff45cd7acaaae5308c9152d0
tiantiancheng <tiantiancheng@baidu.com>
2025-03-03 15:14:01 +0800

    fix: 设备管理容器头部

--- packages/client/src/views/DeviceManagement.tsx
+++ packages/client/src/views/DeviceManagement.tsx
@@ -11,13 +11,15 @@ import {
   TableHead,
   TableRow,
   Link,
-  Typography
+  Typography,
+  CircularProgress
 } from '@mui/material';
 import { ProcessArea, Device } from '../types/device';
 
 const DeviceManagement: React.FC = () => {
   const [currentArea, setCurrentArea] = useState<ProcessArea>(ProcessArea.INLET);
   const [devices, setDevices] = useState<Device[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     console.log('DeviceManagement 组件已挂载');
@@ -26,6 +28,7 @@ const DeviceManagement: React.FC = () => {
   }, [currentArea]);
 
   const fetchDevices = async (area: ProcessArea) => {
+    setLoading(true);
     console.log('开始获取设备数据, 区域:', area);
     try {
       const response = await fetch(`http://localhost:3000/devices?area=${area}`);
@@ -35,6 +38,8 @@ const DeviceManagement: React.FC = () => {
       setDevices(data);
     } catch (error) {
       console.error('获取设备数据失败:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -43,11 +48,21 @@ const DeviceManagement: React.FC = () => {
   };
 
   return (
-    <Box sx={{ width: '100%', p: 3 }}>
-      <Typography variant="h4" sx={{ mb: 3 }}>设备管理</Typography>
+    <Box sx={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      height: 'calc(100vh - 32px)', // 减去 padding
+      overflow: 'hidden'
+    }}>
+      <Typography variant="h4" sx={{ mb: 3, flexShrink: 0 }}>设备管理</Typography>
       
-      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
-        <Tabs value={currentArea} onChange={handleAreaChange}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3, flexShrink: 0 }}>
+        <Tabs 
+          value={currentArea} 
+          onChange={handleAreaChange}
+          variant="scrollable"
+          scrollButtons="auto"
+        >
           <Tab label="进站区" value={ProcessArea.INLET} />
           <Tab label="过滤区" value={ProcessArea.FILTRATION} />
           <Tab label="计量区" value={ProcessArea.METERING} />
@@ -56,8 +71,16 @@ const DeviceManagement: React.FC = () => {
         </Tabs>
       </Box>
 
-      <TableContainer component={Paper}>
-        <Table>
+      <TableContainer 
+        component={Paper} 
+        sx={{ 
+          flex: 1,
+          overflow: 'auto',
+          position: 'relative',
+          minHeight: 200 // 确保即使没有数据也有最小高度
+        }}
+      >
+        <Table stickyHeader>
           <TableHead>
             <TableRow>
               <TableCell>设备编号</TableCell>
@@ -68,8 +91,11 @@ const DeviceManagement: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {devices.map((device) => (
-              <TableRow key={device._id}>
+            {!loading && devices.map((device) => (
+              <TableRow 
+                key={device._id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
                 <TableCell>{device.deviceCode}</TableCell>
                 <TableCell>{device.name}</TableCell>
                 <TableCell>{device.pressureClass}</TableCell>
@@ -83,6 +109,30 @@ const DeviceManagement: React.FC = () => {
             ))}
           </TableBody>
         </Table>
+        
+        {loading && (
+          <Box sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}>
+            <CircularProgress />
+          </Box>
+        )}
+        
+        {!loading && devices.length === 0 && (
+          <Box sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            color: 'text.secondary'
+          }}>
+            <Typography>该区域暂无设备数据</Typography>
+          </Box>
+        )}
       </TableContainer>
     </Box>
   );
